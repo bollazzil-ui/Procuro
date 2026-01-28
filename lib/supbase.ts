b23385b3672a1d43b63f32cbeee0d209dbfe44bf
L.B <bollazzil@gmail.com>
2026-01-28 16:32:31 +0100

    implemented best practices

---
+++ lib/supbase.ts
@@ -0,0 +1,8 @@
+// lib/supabase.ts
+import { createClient } from '@supabase/supabase-js'
+
+// These must be set in your .env.local file
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
